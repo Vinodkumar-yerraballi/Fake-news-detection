c5e1b3f893f31c9e52d10fdc97642f6ceeac60b6
Vinodkumar Yerraballi <98636972+Vinodkumar-yerraballi@users.noreply.github.com>
2022-06-14 16:53:35 +0530

    Update step.sh

--- step.sh
+++ step.sh
@@ -4,10 +4,3 @@ echo "\
 [general]\n\
 email = \"email@domain\"\n\
 " > ~/.streamlit/credentials.toml
-
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
-" > ~/.streamlit/config.toml
